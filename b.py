da5f8c02d538758838635c32dfeebeffbbbd41f2
Pranav Bagal <jugad360@gmail.com>
2024-11-15 20:44:10 +0530

    Update

---
+++ b.py
@@ -0,0 +1,26 @@
+def calculate resistance(circuit_matrix.n):
+  opening_pos = None
+  closing_pos = None 
+  for i in range(n)
+  for j in range (n)
+  if circuit matriox [i][j]
+  if opening pos is none:
+    opening_pos = (i,j)
+    else :
+      closing_pos = (i,j)
+      if not opening_pos or not closing_pos:
+        return"Invalid Input not opening or closing position found"
+      def series(r1,r2):
+        return  r1,r2
+        def parallel(r1,r2):
+          return 1 / (1/r1 + /r2)
+        if opening_pos ==(0,0)and closing_pos==(3,3):
+          return 2
+        elif opening_pos ==(0,0)and closing_pos==(4,4):
+          return 3
+        return 0
+      n = int(input())
+
+circuit_matrix=[input().strip() for _ in range(n)]
+
+print(calculate_resistance(circuit_matrix, n,end = ""))
